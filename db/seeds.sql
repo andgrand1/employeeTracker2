88b7a73140d6f7ab54fa2f1803f0188885b2b3d1
Andy Granda <andygranda@Andys-MacBook-Pro.local>
2023-11-23 11:43:44 -0800

    added departments to seeds

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (id, name) VALUES (1, 'HR');
+
+INSERT INTO department (id, name) VALUES (2, 'Finance');
+
+INSERT INTO department (id, name) VALUES (3, 'IT');
+
+INSERT INTO department (id, name) VALUES (4, 'Marketing');
+
+INSERT INTO department (id, name) VALUES (5, 'Sales');
